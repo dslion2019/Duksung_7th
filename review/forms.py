d1fc3b65f32ecf7ab26b301d5fd5613f896a1ea7
YoonaKim25 <oliviakim0205@gmail.com>
2019-05-30 01:25:36 +0900

    김윤아 rshow와 rpost 연결

--- review/forms.py
+++ review/forms.py
@@ -4,4 +4,4 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['who', 'role', 'write']
+        fields = ['name', 'role', 'body']
--- review/views.py
+++ review/views.py
@@ -1,31 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
 from .forms import ReviewForm
 from .models import Review
+from django.utils import timezone
 
-# Create your views here. 어떤 함수인지 간단히 주석 써주기
-#후기 메인화면
-def rmain(request):
-    return render(request, 'rmain.html')
-    
+# Create your views here.
+# 글쓰기
 def rpost(request):
-    if request.method == "RPOST":
-        form = ReviewForm(request.RPOST)
-        if form.is_valid():
-            review = form.save(commit = False)
-            reveiw.save()
-            return redirect('rshow') 
-    else:
-        form = ReviewForm()
-        return render(request, 'rpost.html', {'form':form})
+    if request.method == "POST":
+        form = ReviewForm(request.POST) 
 
-def redit(request):
-    review = get_object_or_404(Reveiw, pk=pk)
-    if request.method == "RPOST":
-        form = ReviewForm(request.RPOST, instance=review)
-        if form.is_valid():
-            review = form.save(commit = False)
-            reveiw.save()
+        if form.is_valid(): 
+            review = form.save(commit=False) 
+            review.update_date=timezone.now() 
+            review.save()
             return redirect('rshow') 
     else:
-        form = ReviewForm(instance=review)
-        return render(request, 'rpost.html', {'form':form})
+        form = ReviewForm() 
+
+        return render(request, 'rpost.html',{'form' : form})
+
+def rshow(request):
+        reviews = Review.objects.order_by('-id')
+        return render(request, 'rshow.html', {'reviews':reviews})
+
+def redit(request, pk):
+        review = get_object_or_404(Review, pk=pk)
+
+        if request.method == "POST":
+                form = ReviewForm(request.POST, instance=review) 
+
+                if form.is_valid(): 
+                        review = form.save(commit=False) 
+                        review.update_date=timezone.now() 
+                        review.save()
+                        return redirect('rshow') 
+
+        else:
+                form = ReviewForm(instance=review) 
+                return render(request, 'rpost.html',{'form' : form})
+
+def delete(request, pk):
+        review = Review.objects.get(id=pk)
+        review.delete()
+        return redirect('rshow')
