9897bb9ee642949b1cef2ca6bfc134c67ceff094
shfidrla0328 <shfidrla@gmail.com>
2019-11-21 20:40:06 +0900

    loginapp

--- dsWeb/urls.py
+++ dsWeb/urls.py
@@ -1,21 +1,14 @@
-"""dsWeb URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+import loginapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logoutok/', loginapp.views.logoutok, name='logoutok'),
+    path('', loginapp.views.mainpage, name='mainpage'),
+    path('login/', loginapp.views.loginpage, name='login'),
+    path('logout/', loginapp.views.logout, name='logout'),
+    path('signup/', loginapp.views.signuppage, name='signup'),
+    path('signupok/', loginapp.views.singupok, name='signupok'),
+    path('userlist/', loginapp.views.userlist, name='userlist'),
 ]
